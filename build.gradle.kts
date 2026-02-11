7a6368f978d856aacda6f1f68a5f960bdc46b71a
X-rayS5 <61073708+X-rays5@users.noreply.github.com>
2026-02-11 14:03:09 +0100

    Migrate to new sonatype publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,18 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 
-// how to build? run ./gradlew
-// where is the jar? build/lib/ipp-client-kotlin...jar
-
-// update gradle wrapper
-// ./gradlew wrapper --gradle-version 7.6.6
-// gradle 8? should be done when moved to kotlin.jvm plugin 1.9.x (to remove deprecation warnings)
-
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.22" // https://kotlinlang.org/docs/gradle-configure-project.html
-    id("org.jetbrains.dokka") version "1.7.20"      // https://kotlinlang.org/docs/dokka-get-started.html
-    id("org.sonarqube") version "5.0.0.4638"        // https://plugins.gradle.org/plugin/org.sonarqube
-    //id("org.sonarqube") version "3.5.0.2730"      // supports java 8, dropped with 4.1
-    id("maven-publish")                             // https://docs.gradle.org/7.6.2/userguide/publishing_maven.html
-    id("java-library")                              // https://docs.gradle.org/7.6.2/userguide/java_library_plugin.html
-    id("signing")                                   // https://docs.gradle.org/7.6.2/userguide/signing_plugin.html
-    id("jacoco")                                    // https://docs.gradle.org/7.6.2/userguide/jacoco_plugin.html
-    //kotlin("jvm")
+    id("org.jetbrains.kotlin.jvm") version "1.7.22"
+    id("org.jetbrains.dokka") version "1.7.20"
+    id("org.sonarqube") version "5.0.0.4638"
+    id("maven-publish")
+    id("java-library")
+    id("signing")
+    id("jacoco")
+    id("io.github.zenhelix.maven-central-publish") version "0.11.2"
 }
 
-group = "de.gmuth"
-version = "3.5-custom.1"
+group = "dev.scheenen"
+version = "3.5"
 
 repositories {
     mavenCentral()
@@ -28,51 +20,14 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    //testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    //implementation(kotlin("stdlib-jdk8"))
 }
 
-// gradlew clean -x test build publishToMavenLocal
 defaultTasks("assemble")
 
-// https://docs.gradle.org/current/userguide/compatibility.html
+val javaVersion = "1.8"
+val kotlinVersion = "1.7"
+
 tasks.apply {
-
-    // class files versions depend on Kotlin compiler (default is 1.8)
-    // module.json includes "org.gradle.jvm.version" of jdk used by gradle
-    // https://docs.gradle.org/8.5/userguide/variant_attributes.html#sub:jvm_default_attributes
-    // for other projects using gradle this could cause issues like
-    // "Incompatible because this component declares a component,
-    // compatible with Java 21 and the consumer needed a component, compatible with Java 17"
-    // Hence we explicitly set the Kotlin and Java version here
-
-    val javaVersion = "1.8" // JvmTarget.JVM_1_6, default depends on kotlin release
-    val kotlinVersion = "1.7"
-
-    // Kotlin
-    compileKotlin {
-        kotlinOptions {
-            jvmTarget = javaVersion
-            languageVersion = kotlinVersion
-        }
-    }
-    compileTestKotlin {
-        kotlinOptions {
-            jvmTarget = javaVersion
-            languageVersion = kotlinVersion
-        }
-    }
-
-    // Java
-    compileJava {
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-    }
-    compileTestJava {
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-    }
-
     jar {
         manifest {
             attributes(
@@ -86,63 +41,24 @@ tasks.apply {
 
 // ================= PUBLISHING ================
 
-val repo = System.getProperty("repo")
-publishing {
-    repositories {
-        if (repo == "github") {
-            // Github Packages:
-            // gradlew -Drepo=github publish
-            println("> maven repo github")
-            maven {
-                name = "GitHubPackages" // Must match regex [A-Za-z0-9_\-.]+.
-                url = uri("https://maven.pkg.github.com/X-rayS5/ipp-client-kotlin")
-                credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                    password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
-                }
-            }
-        }
-        // Maven Central:
-        // https://central.sonatype.org/publish/release/
-        // gradlew -Drepo=sonatype publish
-        // https://s01.oss.sonatype.org/#stagingRepositories (not Safari)
-        // "Close" and wait "for rule evalutaion"
-        // "Release"
-        if (repo == "sonatype") {
-            println("> maven repo sonatype")
-            maven {
-                name = "ossrh-staging-api"
-                //val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                //val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                //url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-                // val host = "https://s01.oss.sonatype.org"
-                // val path = if (version.toString().endsWith("SNAPSHOT")) "/content/repositories/snapshots/"
-                // else "/service/local/staging/deploy/maven2/"
-                // url = uri(host.plus(path))
-                // new in 2025:
-                // https://central.sonatype.org/publish/publish-portal-snapshots/#publishing-snapshot-releases-for-your-project
-                // https://central.sonatype.com/repository/maven-snapshots/
-                // https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/
-                val isSNAPSHOT = version.toString().endsWith("-SNAPSHOT")
-                url = uri(
-                    if (isSNAPSHOT) "https://central.sonatype.com/repository/maven-snapshots/"
-                    else "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
-                )
-                println("> publish.url: $url")
-                credentials {
-                    username = project.findProperty("ossrh.username") as String?
-                    password = project.findProperty("ossrh.password") as String?
-                }
-            }
-        }
-    }
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
 
+// Configure Dokka for javadocJar
+tasks.named<Jar>("javadocJar") {
+    from(tasks.named<DokkaTask>("dokkaJavadoc"))
+}
+
+// Configure Gradle Central Publisher Plugin
+mavenCentralPublish {
     publications {
-        create<MavenPublication>("ippclient") {
+        create("ippclient") {
             from(components["java"])
             pom {
                 name.set("ipp client library")
-                description.set("A client implementation of the ipp protocol, RFCs 8010, 8011, 3995 and 3996")
+                description.set("A client implementation of the ipp protocol written in kotlin ")
                 url.set("https://github.com/X-rays5/ipp-client-kotlin")
                 licenses {
                     license {
@@ -160,54 +76,78 @@ publishing {
                 scm {
                     connection.set("scm:git:git://github.com/X-rays5/ipp-client-kotlin.git")
                     developerConnection.set("scm:git:git@github.com:X-rays5/ipp-client-kotlin.git")
-                    url.set("https://github.com/X-rays5/ipp-client-kotlin")
+                    url.set("https://github.com/X-rays5/ipp-client-kotlin.git")
+                }
+            }
+        }
+    }
+
+    credentials {
+        username.set(findProperty("centralTokenUser") as String? ?: System.getenv("CENTRAL_TOKEN_USER"))
+        password.set(findProperty("centralTokenPass") as String? ?: System.getenv("CENTRAL_TOKEN_PASS"))
+    }
+
+    publishingType.set(PublishingType.USER_MANAGED) // or AUTOMATIC
+}
+
+publishing {
+    repositories {
+        mavenCentralPortal {
+            credentials {
+                username = findProperty("centralTokenUser") as String? ?: System.getenv("CENTRAL_TOKEN_USER")
+                password = findProperty("centralTokenPass") as String? ?: System.getenv("CENTRAL_TOKEN_PASS")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("ippclient") {
+            from(components["java"])
+
+            pom {
+                name = "ipp-client-kotlin"
+                description = "A client implementation of the ipp protocol written in kotlin "
+                url = "https://github.com/X-rays5/ipp-client-kotlin"
+
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://raw.githubusercontent.com/X-rays5/ipp-client-kotlin/refs/heads/master/LICENSE"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "X-rayS5"
+                        name = "X-ray"
+                        email = "61073708+X-rays5@users.noreply.github.com"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/X-rays5/ipp-client-kotlin.git"
+                    developerConnection = "scm:git:git@github.com:X-rays5/ipp-client-kotlin.git"
+                    url = "https://github.com/X-rays5/ipp-client-kotlin"
                 }
             }
         }
     }
 }
 
-java {
-    withSourcesJar()
+// Signing
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
 
-repo?.run { // tasks for publishing on maven repo
-    // ====== signing ======
-    // set gradle.properties
-    // signing.keyId
-    // signing.password
-    // signing.secretKeyRingFile
-    // gradle signIppclientPublication
-    signing {
-        sign(publishing.publications["ippclient"])
-    }
-    // ======  produce sources.jar and javadoc.jar ======
-    java {
-        //withSourcesJar()
-        withJavadocJar()
-    }
-    // configure task javadocJar to take javadoc generated by dokkaJavadoc
-    tasks.named<Jar>("javadocJar") {
-        from(tasks.named<DokkaTask>("dokkaJavadoc"))
-    }
-}
+// ================= SONARQUBE / JACOCO =================
 
-// ====== analyse code with SonarQube ======
-
-val isRunningOnGithub = System.getenv("CI")?.toBoolean() ?: false
-println("isRunningOnGithub=$isRunningOnGithub")
-
-// https://docs.gradle.org/current/userguide/jacoco_plugin.html
 jacoco {
-    // https://mvnrepository.com/artifact/org.jacoco/jacoco-maven-plugin
     toolVersion = "0.8.11"
 }
 
-// required for sonarqube code coverage
-// https://docs.sonarqube.org/latest/analysis/test-coverage/java-test-coverage
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
-    // https://stackoverflow.com/questions/67725347/jacoco-fails-on-gradle-7-0-2-and-kotlin-1-5-10
     reports {
         xml.required.set(true)
         csv.required.set(false)
@@ -215,38 +155,9 @@ tasks.jacocoTestReport {
     }
 }
 
-// gradle test jacocoTestReport sonar
-// https://docs.sonarqube.org/latest/analysis/scan/sonarscanner-for-gradle/
-// configure token with 'publish analysis' permission in file ~/.gradle/gradle.properties:
-// systemProp.sonar.login=<token>
-//sonar {
-//    properties {
-//        property("sonar.host.url", "https://sonarcloud.io")
-//        property("sonar.projectKey", "gmuth_ipp-client-kotlin")
-//        property("sonar.organization", "gmuth")
-//        //property("sonar.verbose", "true")
-//        //property("sonar.junit.reportPaths", "build/test-results/test")
-//        //property("sonar.jacoco.reportPaths", "build/jacoco/test.exec")
-//        //property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/")
-//    }
-//}
-
-// https://docs.sonarsource.com/sonarqube/latest/analyzing-source-code/scanners/sonarscanner-for-gradle/
 tasks.sonar {
-    dependsOn(tasks.jacocoTestReport) // for coverage
+    dependsOn(tasks.jacocoTestReport)
 }
 
-// ====== fat jar ======
-
-//tasks.register("fatJar", Jar::class) {
-//    description = "Generates a fat jar for this project."
-//    group = JavaBasePlugin.DOCUMENTATION_GROUP
-//    manifest.attributes["Main-Class"] = "CopyNewJobsKt"
-//    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-//    from(configurations.runtimeClasspath.get().map(::zipTree))
-//    with(tasks.jar.get())
-//}
-
-//kotlin {
-//    jvmToolchain(17)
-//}
+val isRunningOnGithub = System.getenv("CI")?.toBoolean() ?: false
+println("isRunningOnGithub=$isRunningOnGithub")
